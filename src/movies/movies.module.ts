4b5684784e9319e8749437c90e5315b51a544722
Bharath-0810 <bharath.ps0810@gmail.com>
2025-12-10 16:42:42 +0000

    feat: add movie schema and integrate with Mongoose module

--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { MoviesController } from './movies.controller';
-
+import { Movie, MovieSchema } from './schemas/movie.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Movie.name, schema: MovieSchema }])],
   providers: [MoviesService],
   controllers: [MoviesController]
 })
